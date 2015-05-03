0ed1b7ec6d41c6aede27cd6ca627e722ad4bd9c1
Liz Tom <liz.tom@gmail.com>
2015-05-03 00:31:35 -0400

    add side menu

--- app/assets/javascripts/drag.js
+++ app/assets/javascripts/drag.js
@@ -1,6 +1,7 @@
 $( document ).ready(function() {
 
     var ingredients = document.getElementById('js-ingredients');
+    var ingredients2 = document.getElementById('js-ingredients2');
     var container = document.getElementById('js-container');
 
     Sortable.create(ingredients, {
@@ -10,6 +11,11 @@ $( document ).ready(function() {
 
     // sort: false
     Sortable.create(container, {
+      group: "sorting",
+      sort: true,
+    });
+
+    Sortable.create(ingredients2, {
       group: "sorting",
       sort: true,
     });
